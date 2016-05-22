2ffe408aef872d20637364d97659994be4b65f38
Nicholas Virkman <nvirkman@gmail.com>
2016-05-22 22:17:34 +0200

    Lysstyring virker nu.

--- Unit.cpp
+++ Unit.cpp
@@ -9,6 +9,7 @@ Unit::Unit(int id_, QString unitName_) {
     if(id_ !=NULL && unitName_ != NULL){
         id = id_;
         unitName = unitName_;
+        volume = 70;
     } else {
         exit(0);
     }
