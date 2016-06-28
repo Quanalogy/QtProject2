41c5448e03e523508a55da7d104412770434ad23
Munke <aalsing@gmail.com>
2016-06-28 13:28:10 +0200

    reading at a new point

--- CSender.cpp
+++ CSender.cpp
@@ -33,7 +33,7 @@ bool CSender::sendToDE2(QString rightCode, QString tryCode){
     int t_size = tryCode.length();
     //int rightBinCode[8*r_size];
     //int tryBinCode[8*t_size];
-    int completeSIZE = 12*(r_size+t_size)-3;
+    int completeSIZE = 12*(r_size+t_size);
 
     int completeCode [completeSIZE] = {0};
     cout << "The size of r_size: " << r_size << " = The size of t_size: " << t_size
@@ -81,7 +81,7 @@ bool CSender::sendToDE2(QString rightCode, QString tryCode){
             digitalWrite(SERIALOUT,LOW);
             cout << "0" ;
         }
-        if(!k%22){
+        if(k!= 0 && !k%22){
             cout << "Reading now: ";
             digitalWrite(readingPIN, HIGH);
             delayMicroseconds(180);
