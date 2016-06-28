b62149c4129bddb7e2e2f868b605d323ab6f85c1
Munke <aalsing@gmail.com>
2016-06-28 12:43:10 +0200

    Rewrite of the code to fit the complete array with 1110 between each burst

--- CSender.cpp
+++ CSender.cpp
@@ -31,7 +31,7 @@ bool CSender::sendToDE2(QString rightCode, QString tryCode){
     int t_size = tryCode.length();
     int rightBinCode[8*r_size];
     int tryBinCode[8*t_size];
-    int completeSIZE = 8*r_size+8*t_size+2+(4*(r_size+t_size)/2);
+    int completeSIZE = 8*r_size+8*t_size+1+(2*(r_size+t_size));
     int completeCode [completeSIZE] = {0};
     cout << "The size of r_size: " << r_size << " = The size of t_size: " << t_size
             << "The size of completeCode: " << completeSIZE << endl;
