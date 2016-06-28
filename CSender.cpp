23e5b43c840d2038e93fc11e2a29de2c7f27c72e
Munke <aalsing@gmail.com>
2016-06-28 17:43:10 +0200

    bigger delay and no permanen high

--- CSender.cpp
+++ CSender.cpp
@@ -92,7 +92,7 @@ bool CSender::sendToDE2(QString rightCode, QString tryCode){
             ++i;
             cout << "Reading now: ";
             //digitalWrite(readingPIN, HIGH);
-            delayMicroseconds(215);
+            delayMicroseconds(208);
             digitalWrite(readingPIN, HIGH);
             if(!digitalRead(SERIALIN)){
                 cout << "The response is false!" << endl;
